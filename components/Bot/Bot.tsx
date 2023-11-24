d059134a9b0cff9b76e3a2f42f1c3bc24b2252d3
Ashutosh <ashutoshnautiyal94@gmail.com>
2023-11-25 02:30:17 +0530

    done with the chatbhot

---
+++ components/Bot/Bot.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React from "react";
+
+import Chatbot from "react-chatbot-kit";
+import "react-chatbot-kit/build/main.css";
+
+import config from "./config";
+import MessageParser from "./MessageParser";
+import ActionProvider from "./ActionProvider";
+
+import "./bot.css";
+type Props = {};
+
+const Bot = (props: Props) => {
+  return (
+    <div>
+      {" "}
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />{" "}
+    </div>
+  );
+};
+
+export default Bot;
---
+++ components/Bot/config.js
@@ -0,0 +1,53 @@
+import { createChatBotMessage } from 'react-chatbot-kit';
+import Avatar from './Avatar';
+import StartBtn from './StartBtn';
+import StartSlow from './StartSlow';
+// import data from './data';
+import DipslayImage from './DisplayImage';
+
+import data from './data'
+
+
+const config = {
+    botName: "AgeDrive Explorer",
+    initialMessages: [createChatBotMessage(`Welcome to RideAge Advisor!`, {
+        widget: "startBtn"
+    })],
+    customComponents: {
+        botAvatar: (props) => <Avatar {...props} />,
+        // userAvatar: (props) => <Avatar {...props} />,
+    },
+    state: {
+        checker: null,
+        data,
+        userData: {
+            name: "",
+            age: 0,
+            category: "",
+            product: {
+                name: "",
+                link: "",
+                imageUrl: ""
+            }
+        }
+    },
+    widgets: [
+        {
+            widgetName: "startBtn",
+            widgetFunc: (props) => <StartBtn {...props} />,
+        },
+        {
+            widgetName: "startSlow",
+            widgetFunc: (props) => <StartSlow {...props} />,
+        },
+        {
+            widgetName: "finalImage",
+            widgetFunc: (props) => <DipslayImage {...props} />,
+        },
+    ]
+};
+
+export default config;
+
+
+
