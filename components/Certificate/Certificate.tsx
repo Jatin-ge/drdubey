64a42ac8023f6fd3b5ed89aa47fbfd690e1b5475
Ashutosh <ashutoshnautiyal94@gmail.com>
2023-12-28 14:17:06 +0530

    minor changes

--- components/Certificate/Certificate.tsx
+++ components/Certificate/Certificate.tsx
@@ -5,10 +5,15 @@ type Props = {};
 
 const Certificate = (props: Props) => {
   return (
-    <div className="flex items-center justify-center mb-8">
+    <div className="flex flex-col items-center justify-center mb-8 xl:space-x-5">
+      <h1 className=" text-center my-8 w-[80%] font-extrabold text-gray-700 text-xl lg:text-3xl xl:text-6xl leading-relaxed ">
+        <span className="text-primary">Dr. Dheeraj</span> got awarded for{" "}
+        <span className="text-primary">Most Trusted </span>Joint Replacement
+        Surgeon of the Year
+      </h1>
       <Image
-        width={800}
-        height={800}
+        width={900}
+        height={900}
         src="/assets/images/certi2.jpeg"
         alt="certi"
       />
