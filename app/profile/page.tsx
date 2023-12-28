7425a8586e28f93d05c611a90d3f6c9b1b1dd268
AyushChamoli961 <ayushchamoli001@gmail.com>
2023-12-28 14:00:18 +0530

    profile bug fix

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -8,11 +8,11 @@ import Navbar from "@/components/Navbar/navbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useModal } from "@/hooks/use-modal-store";
+import SelectCity from "@/components/ui/select-city";
 
 const Page = async () => {
 
   const AuthProfile = await currentProfile();
-  const { onOpen } = useModal();
 
   if (!AuthProfile) {
     console.error("Profile is undefined");
@@ -91,17 +91,7 @@ const Page = async () => {
           </div>
         </>
       ) : (
-        <div className="text-lg  md:text-3xl font-semibold text-center m-12">
-          <div className="my-8">
-            Book an Appointment to create Patient Profile !
-          </div>
-          <Button
-            onClick={() => onOpen("selectCity")}
-            className="p-2 border  relative inline-flex justify-center items-center gap-x-3 text-center bg-primary hover:bg-blue-700  border-primary text-xl lg:text-base text-white  rounded-md focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800"
-          >
-            Book an appointment
-          </Button>
-        </div>
+        <SelectCity/>
       )}
     </>
   );
