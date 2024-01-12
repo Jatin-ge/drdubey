4ad4b6b7100f43f4a9fdc885666aca4b92152dc6
Ashutosh <ashutoshnautiyal94@gmail.com>
2024-01-12 21:34:16 +0530

    done

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,34 +1,19 @@
 import Footer from "@/components/Footer/Footer";
 import Gallery1 from "@/components/Gallery/Gallery1";
-import Gallery4 from "@/components/Gallery4/Gallery4";
+import Gallery from "@/components/Gallery4/NewGallery";
 import Navbar from "@/components/Navbar/navbar";
+import { db } from "@/lib/db";
 import React from "react";
 
 type Props = {};
 
-const images = [
-  "/assets/images/one.JPG",
-  "/assets/images/dubay/img1.webp",
-  "/assets/images/main.JPG",
-  "/assets/images/two.JPG",
-  "/assets/images/dubay/p3.jpg",
-  "/assets/images/dubay/p4.webp",
-  "/assets/images/three.JPG",
-  "/assets/images/four.JPG",
-  "/assets/images/dubay/g14.jpg",
-  "/assets/images/dubay/img2.jpg",
-  "/assets/images/dubay/img5.webp",
-  "/assets/images/dubay/img7.jpg",
-  "/assets/images/dubay/img4.webp",
-  "/assets/images/dubay/img8.jpg",
-  "/assets/images/dubay/p5.webp",
-];
+const page = async (props: Props) => {
+  const newimages = await db.image.findMany({});
 
-const page = (props: Props) => {
   return (
     <div>
       <Navbar />
-      <Gallery4 images={images} />
+      <Gallery images={newimages} />
       <Footer />
     </div>
   );
